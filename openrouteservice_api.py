9fa2b6e24b03c762ddd6351c8ff21ecb1198bfec
ruhaans05 <143101275+ruhaans05@users.noreply.github.com>
2025-07-24 11:41:10 -0400

    Update openrouteservice_api.py

--- openrouteservice_api.py
+++ openrouteservice_api.py
@@ -6,6 +6,7 @@ import streamlit as st
 import math
 import polyline as pl
 
+
 ORS_API_KEY = os.getenv("ORS_API_KEY")
 client = openrouteservice.Client(key=ORS_API_KEY)
 
@@ -36,29 +37,28 @@ def get_driving_route(origin_coords, dest_coords, avoid_tolls=False):
         "traffic_color": "gray"
     }
 
+
 def haversine(coord1, coord2):
     R = 6371e3  # Earth radius in meters
-    lat1 = math.radians(coord1[0])
-    lon1 = math.radians(coord1[1])
-    lat2 = math.radians(coord2[0])
-    lon2 = math.radians(coord2[1])
-    dlat = lat2 - lat1
-    dlon = lon2 - lon1
+    lat1, lon1 = math.radians(coord1[0]), math.radians(coord1[1])
+    lat2, lon2 = math.radians(coord2[0]), math.radians(coord2[1])
+    dlat, dlon = lat2 - lat1, lon2 - lon1
 
-    a = math.sin(dlat/2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon/2)**2
-    return R * 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-
+    a = math.sin(dlat/2)**2 + math.cos(lat1)*math.cos(lat2)*math.sin(dlon/2)**2
+    return R * 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
 
 def get_interval_coords(polyline_str, num_intervals):
     coords = pl.decode(polyline_str)
     total_dist = 0
     dists = [0]
 
+    # Compute cumulative distances
     for i in range(1, len(coords)):
         dist = haversine(coords[i - 1], coords[i])
         total_dist += dist
         dists.append(total_dist)
 
+    # Target distance spacing
     step_dist = total_dist / (num_intervals + 1)
     result = []
     next_target = step_dist
@@ -72,23 +72,6 @@ def get_interval_coords(polyline_str, num_intervals):
 
     return result
 
-def reverse_geocode(lat, lon):
-    url = "https://nominatim.openstreetmap.org/reverse"
-    params = {
-        "format": "json",
-        "lat": lat,
-        "lon": lon,
-        "zoom": 10,
-        "addressdetails": 1
-    }
-    headers = {"User-Agent": "TransportNYC-App"}
-
-    res = requests.get(url, params=params, headers=headers)
-    if res.status_code == 200:
-        data = res.json()
-        addr = data.get("address", {})
-        return addr.get("county") or addr.get("city") or addr.get("state") or "Unknown location"
-    return "Unknown location"
 
 def search_nearby_pois(lat, lon, kind, delta=0.1):
     keyword_map = {
